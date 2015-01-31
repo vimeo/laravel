7a67a604f59deb5ca005f056b3f694bf791c5358
Vincent Klaiber <vincentklaiber@gmail.com>
2015-01-31 20:46:38 +0100

    Add @GrahamCampbell service provider tests.

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,9 @@
+<?php namespace Vinkla\Tests\Vimeo;
+
+use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
+
+class ServiceProviderTest extends AbstractTestCase {
+
+	use ServiceProviderTestCaseTrait;
+
+}
