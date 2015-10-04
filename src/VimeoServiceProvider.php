e6d36ed04676282a2e39f482df9bd7ebf917aaf9
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-04 14:56:48 +0200

    Improved Lumen support

--- src/VimeoServiceProvider.php
+++ src/VimeoServiceProvider.php
@@ -29,20 +29,24 @@ class VimeoServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig();
+        $this->setupConfig($this->app);
     }
 
     /**
      * Setup the config.
      *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
      * @return void
      */
-    protected function setupConfig()
+    protected function setupConfig(Application $app)
     {
         $source = realpath(__DIR__.'/../config/vimeo.php');
 
         if (class_exists('Illuminate\Foundation\Application', false)) {
             $this->publishes([$source => config_path('vimeo.php')]);
+        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+            $app->configure('vimeo');
         }
 
         $this->mergeConfigFrom($source, 'vimeo');
