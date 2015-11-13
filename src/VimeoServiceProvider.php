d01d12ef9db00589aa4242ee22073ca9c74bd29c
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-13 09:08:11 +0100

    Improved environment specific code

--- src/VimeoServiceProvider.php
+++ src/VimeoServiceProvider.php
@@ -12,7 +12,9 @@
 namespace Vinkla\Vimeo;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 use Vimeo\Vimeo;
 
 /**
@@ -43,9 +45,9 @@ class VimeoServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/vimeo.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('vimeo.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('vimeo');
         }
 
