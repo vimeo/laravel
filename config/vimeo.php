cb23e4302226d096f405c9c42a2cc0502d1d337b
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-02-03 19:20:58 -0600

    Update access tokens default value to null
    Update access tokens default value to null.

--- config/vimeo.php
+++ config/vimeo.php
@@ -48,13 +48,13 @@ return [
         'main' => [
             'client_id' => env('VIMEO_CLIENT', 'your-client-id'),
             'client_secret' => env('VIMEO_SECRET', 'your-client-secret'),
-            'access_token' => env('VIMEO_ACCESS', 'your-access-token'),
+            'access_token' => env('VIMEO_ACCESS', null),
         ],
 
         'alternative' => [
             'client_id' => env('VIMEO_ALT_CLIENT', 'your-alt-client-id'),
             'client_secret' => env('VIMEO_ALT_SECRET', 'your-alt-client-secret'),
-            'access_token' => env('VIMEO_ALT_ACCESS', 'your-alt-access-token'),
+            'access_token' => env('VIMEO_ALT_ACCESS', null),
         ],
 
     ],
